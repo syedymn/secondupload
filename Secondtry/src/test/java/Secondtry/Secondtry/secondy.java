84eb7f06c13113edd6a1583fe045e4bed11a1cc3
syedy <syedy@DESKTOP-TAV9QK2.mynetworksettings.com>
2025-01-02 00:22:30 -0500

    Second try 2025

---
+++ Secondtry/src/test/java/Secondtry/Secondtry/secondy.java
@@ -0,0 +1,44 @@
+package Secondtry.Secondtry;
+
+import org.testng.annotations.Test;
+
+public class secondy {
+	
+@Test
+	
+		public static void method() {
+		
+	System.out.println("My first Writing");
+	String name = "Mark";
+	int age= 35;
+	char grade= 'A';
+	double cgpa = 3.75;
+	Boolean Pass = true;
+
+	System.out.println("My first Writing topic was " +name);
+	System.out.println("his was "+ age);
+	System.out.println("his gave the test and got "+ grade);
+	System.out.println("so an A is equal to "+cgpa);
+	System.out.println("which is the pass mark "+ Pass);
+
+	age= 45;
+	cgpa= 4.00;
+	grade= 'B';
+
+	System.out.println("his was "+ age);
+
+	Pass= false;
+
+	System.out.println("which is the pass mark "+ Pass);
+
+	name="Alex";
+	age= 29;
+	System.out.println("My first Writing topic was " +name);
+	System.out.println("his was "+ age);
+
+
+		}
+	 
+	}
+
+
